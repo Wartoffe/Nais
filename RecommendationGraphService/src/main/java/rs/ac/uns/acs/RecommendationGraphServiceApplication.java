6888749a027bbfb1ed828ccedae09462d17929d4
pesict2612 <pesic.teodora2612@gmail.com>
2026-04-28 09:52:53 +0200

    povezivanje reccomendation servisa

--- RecommendationGraphService/src/main/java/rs/ac/uns/acs/RecommendationGraphServiceApplication.java
+++ RecommendationGraphService/src/main/java/rs/ac/uns/acs/RecommendationGraphServiceApplication.java
@@ -2,8 +2,11 @@ package rs.ac.uns.acs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication(scanBasePackages = "rs.ac.uns.acs")
+@EnableDiscoveryClient
 public class RecommendationGraphServiceApplication {
 
 	public static void main(String[] args) {
