bb0a53988de2ca182b104d8b0dcdabc7694b8ab8
neshko31 <neca.lukic100@gmail.com>
2026-04-28 14:00:42 +0200

    Povezan još jedan servis

--- GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/GraphDatabaseServiceApplication.java
+++ GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/GraphDatabaseServiceApplication.java
@@ -2,8 +2,10 @@ package rs.ac.uns.acs.nais.GraphDatabaseService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GraphDatabaseServiceApplication {
 
 	public static void main(String[] args) {
