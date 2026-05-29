1de646fdfa9be8cd3bd792d818d78d83f26786e3
Wartoffe <srdjansancanin123@gmail.com>
2026-05-29 21:48:45 +0200

    2/8 cruds work

--- ElasticSearch/search/src/main/java/nais/search/controller/BookController.java
+++ ElasticSearch/search/src/main/java/nais/search/controller/BookController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/books")
+@RequestMapping("/api/books")
 public class BookController {
 
     private final BookService bookService;
